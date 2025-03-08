bb94af0fde0bd739f6d5e69e4366c8d779f7eaab
caiohrgm <caio.medeiros@ccc.ufcg.edu.br>
2025-03-07 21:48:13 -0300

    Working on home.

--- project/src/components/Header.tsx
+++ project/src/components/Header.tsx
@@ -1,3 +1,5 @@
+import linkedin_logo from "../assets/linkedin_logo_colored.png";
+
 export default function Header() {
   return (
     <header className="bg-[#1B1B1B] py-4 px-8 flex justify-between items-center">
@@ -19,14 +21,8 @@ export default function Header() {
         </a>
       </nav>
       <div className="flex space-x-4 text-[#9C9C9C]">
-        <a href="#" className="hover:text-white">
-          in
-        </a>
-        <a href="#" className="hover:text-white">
-          Bē
-        </a>
-        <a href="#" className="hover:text-white">
-          🐦
+        <a href="https://www.linkedin.com/in/caiohmedeiros/" className="hover:text-white" target="_blank" rel="noopener noreferrer">
+        <img src={linkedin_logo} alt="SENAI-PB" className="w-6 h-4 mr-2 hover:opacity-80" />
         </a>
       </div>
     </header>
--- project/src/components/Hero.tsx
+++ project/src/components/Hero.tsx
@@ -2,11 +2,11 @@ import profileImage from "../assets/profile.png";
 
 export default function Hero() {
   return (
-    <section className="flex flex-col-reverse md:flex-row items-center justify-center flex-grow px-8 py-12 md:py-24 bg-[#080808]">
+    <section className="flex flex-col-reverse md:flex-row items-center justify-center flex-grow px-8 py-12 md:py-24 bg-[#080808] mt-20">
       <div className="text-center md:text-left max-w-lg py">
         <h1 className="text-5xl font-bold font-raleway">Caio Medeiros</h1>
         <p className="text-[#9C9C9C] mt-4 font-mono">
-          Software Engineer and Data Scientist, focused on back-end.
+          Software Engineer and Data Scientist, experienced with back-end, data analasys and R&D projects.
         </p>
         <button className="border bg-[#3F8E00] border-[#62BA1B] mt-6 p-4 rounded-md font-mono">
           Let´s get started
