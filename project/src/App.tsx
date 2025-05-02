11615bac30654e30761b3071f774098bc6477b36
caiohrgm <caio.medeiros@ccc.ufcg.edu.br>
2025-05-02 16:15:12 -0300

    Upload update son the branch

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -3,19 +3,16 @@ import Layout from "./components/NavBarLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
-import ProjectPage from "./components/Projects/ProjectsPages/ProjectPage"
-// import Contact from "./pages/Contact";
+import ProjectTemplate from "./components/Projects/ProjectTemplate";
 
 export default function App() {
   return (
     <Routes>
-      {/* Wrap all pages inside the Layout */}
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="projects" element={<Projects />} />
-        <Route path="projects/:slug" element={<ProjectPage />} />
-
+        <Route path="/projects/:projectId" element={<ProjectTemplate />} />
         {/* <Route path="contact" element={<Contact />} /> */}
       </Route>
     </Routes>
